77a26b66b63cfa7b0ff5a23cc5a1d7144ee19f41
Sid Sherpa <siddoesdesigns@gmail.com>
2022-05-04 13:53:22 +0545

    first commit

---
+++ js/main.js
@@ -0,0 +1,71 @@
+
+$(document).ready(function(){
+  $(".owl-carousel").owlCarousel({
+      margin: 20,
+      items: 3,
+      dots: true,
+      responsive: {
+        0: {
+          items: 1
+        },
+        768 : {
+          items: 2
+        },
+        1366 : {
+          items: 3
+        }
+      }
+  });
+});
+
+$(document).ready(function(){
+
+  $("#ex1-tab-1").click(function(){
+    $("#ex1-tab-1").addClass("active")
+    $("#ex1-tab-2").removeClass("active")
+    $("#ex1-tab-3").removeClass("active")
+    $("#ex1-pills-1").addClass("show")
+    $("#ex1-pills-1").addClass("active")
+    $("#ex1-pills-2").removeClass("show")
+    $("#ex1-pills-2").removeClass("active")
+    $("#ex1-pills-3").removeClass("show")
+    $("#ex1-pills-3").removeClass("active")
+  })
+
+  $("#ex1-tab-2").click(function(){
+    $("#ex1-tab-2").addClass("active")
+    $("#ex1-tab-1").removeClass("active")
+    $("#ex1-tab-3").removeClass("active")
+    $("#ex1-pills-1").removeClass("show")
+    $("#ex1-pills-1").removeClass("active")
+    $("#ex1-pills-2").addClass("show")
+    $("#ex1-pills-2").addClass("active")
+    $("#ex1-pills-3").removeClass("show")
+    $("#ex1-pills-3").removeClass("active")
+  })
+
+  $("#ex1-tab-3").click(function(){
+    $("#ex1-tab-3").addClass("active")
+    $("#ex1-tab-1").removeClass("active")
+    $("#ex1-tab-2").removeClass("active")
+    $("#ex1-pills-1").removeClass("show")
+    $("#ex1-pills-1").removeClass("active")
+    $("#ex1-pills-2").removeClass("show")
+    $("#ex1-pills-2").removeClass("active")
+    $("#ex1-pills-3").addClass("show")
+    $("#ex1-pills-3").addClass("active")
+  })
+})
+
+window.onload=function(){
+
+  document.getElementById("ex1-pills-1").addEventListener("click", clicker)
+  function clicker(){
+    console.log("abc")
+  }
+}
+
+AOS.init({
+  easing: 'ease-in-out-sine'
+});
+
