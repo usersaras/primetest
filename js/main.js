3f066cde653274f7e9e75e3b01351a30856a3938
usersaras <saras.sthapitshrestha@gmail.com>
2022-05-09 13:56:12 +0545

    headr images and aos fix

--- js/main.js
+++ js/main.js
@@ -1,34 +1,23 @@
-console.log("check");
+$(document).ready(function () {
 
 
-$(document).ready(function(){
-  document.getElementById("main").style.display = "none";
-
-$(window).load(function(){
-  $("#loader").fadeOut("slow");
-  $('#loader').hide();
-  $("#main").fadeIn("slow");
-  AOS.init()
-  AOS.refresh()
-})
-
-
+  AOS.init();
 
   $(".owl-carousel").owlCarousel({
-      margin: 20,
-      items: 3,
-      dots: true,
-      responsive: {
-        0: {
-          items: 1
-        },
-        768 : {
-          items: 2
-        },
-        1366 : {
-          items: 3
-        }
+    margin: 20,
+    items: 3,
+    dots: true,
+    responsive: {
+      0: {
+        items: 1
+      },
+      768: {
+        items: 2
+      },
+      1366: {
+        items: 3
       }
+    }
   });
 
   $(".owl-carousel-2").owlCarousel({
@@ -39,60 +28,85 @@ $(window).load(function(){
       0: {
         items: 1
       },
-      768 : {
+      768: {
         items: 2
       },
-      1366 : {
+      1366: {
         items: 3
       }
     }
+  });
 });
-});
 
+$(document).ready(function () {
 
-
-
-
-$(document).ready(function(){
-
-  $("#ex1-tab-1").click(function(){
+  $("#ex1-tab-1").click(function () {
     $("#ex1-tab-1").addClass("active")
     $("#ex1-tab-2").removeClass("active")
     $("#ex1-tab-3").removeClass("active")
+    $("#ex1-tab-4").removeClass("active")
+
     $("#ex1-pills-1").addClass("show")
     $("#ex1-pills-1").addClass("active")
     $("#ex1-pills-2").removeClass("show")
     $("#ex1-pills-2").removeClass("active")
     $("#ex1-pills-3").removeClass("show")
     $("#ex1-pills-3").removeClass("active")
+    $("#ex1-pills-4").removeClass("show")
+    $("#ex1-pills-4").removeClass("active")
   })
 
-  $("#ex1-tab-2").click(function(){
+  $("#ex1-tab-2").click(function () {
     $("#ex1-tab-2").addClass("active")
     $("#ex1-tab-1").removeClass("active")
     $("#ex1-tab-3").removeClass("active")
+    $("#ex1-tab-4").removeClass("active")
+
     $("#ex1-pills-1").removeClass("show")
     $("#ex1-pills-1").removeClass("active")
     $("#ex1-pills-2").addClass("show")
     $("#ex1-pills-2").addClass("active")
     $("#ex1-pills-3").removeClass("show")
     $("#ex1-pills-3").removeClass("active")
+    $("#ex1-pills-4").removeClass("show")
+    $("#ex1-pills-4").removeClass("active")
   })
 
-  $("#ex1-tab-3").click(function(){
+  $("#ex1-tab-3").click(function () {
     $("#ex1-tab-3").addClass("active")
     $("#ex1-tab-1").removeClass("active")
     $("#ex1-tab-2").removeClass("active")
+    $("#ex1-tab-4").removeClass("active")
+
     $("#ex1-pills-1").removeClass("show")
     $("#ex1-pills-1").removeClass("active")
     $("#ex1-pills-2").removeClass("show")
     $("#ex1-pills-2").removeClass("active")
     $("#ex1-pills-3").addClass("show")
     $("#ex1-pills-3").addClass("active")
+    $("#ex1-pills-4").removeClass("show")
+    $("#ex1-pills-4").removeClass("active")
+  })
+
+  $("#ex1-tab-4").click(function () {
+    $("#ex1-tab-1").addClass("active")
+    $("#ex1-tab-2").removeClass("active")
+    $("#ex1-tab-3").removeClass("active")
+
+    $("#ex1-tab-4").addClass("active")
+    $("#ex1-pills-4").addClass("show")
+    $("#ex1-pills-4").addClass("active")
+
+    $("#ex1-tab-1").removeClass("active")
+    $("#ex1-tab-2").removeClass("active")
+    $("#ex1-pills-1").removeClass("show")
+    $("#ex1-pills-1").removeClass("active")
+    $("#ex1-pills-2").removeClass("show")
+    $("#ex1-pills-2").removeClass("active")
+    $("#ex1-pills-3").removeClass("show")
+    $("#ex1-pills-3").removeClass("active")
+    
   })
 })
 
-AOS.init({
-  easing: 'ease-in-out-sine'
-});
 
